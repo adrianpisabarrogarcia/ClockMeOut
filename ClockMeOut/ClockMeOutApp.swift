a9212ff9c2163e699cf5c26b459c9f3bc7b15bae
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2023-10-17 01:26:25 +0200

    Initial Commit

---
+++ ClockMeOut/ClockMeOutApp.swift
@@ -0,0 +1,20 @@
+//
+//  ClockMeOutApp.swift
+//  ClockMeOut
+//
+//  Created by Adrián Pisabarro García on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ClockMeOutApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
